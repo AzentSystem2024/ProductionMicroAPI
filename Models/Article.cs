44a3b720cb6c85bc8c1aad2927daf4df54126c91
Bhavana-BS <bhavana@renazentit.com>
2025-06-20 13:57:48 +0530

    Changes in the select of article master

--- Models/Article.cs
+++ Models/Article.cs
@@ -19,8 +19,8 @@
         public int NEW_ARRIVAL_DAYS { get; set; }
         public bool IS_STOPPED { get; set; }
         public int SUPPLIER_ID { get; set; }
-        public bool IsComponent { get; set; }
-        public int? ComponentArticleID { get; set; }
+        public bool IS_COMPONENT { get; set; }
+        public int? COMPONENT_ARTICLE_ID { get; set; }
     }
 
     public class ArticleResponse
@@ -44,30 +44,46 @@
         public string PART_NO { get; set; }
         public string ALIAS_NO { get; set; }
         public int UNIT_ID { get; set; }
+        public string UnitCode { get; set; }
         public int ARTICLE_TYPE { get; set; }
-        public string? ARTICLE_TYPE_NAME { get; set; }
+        public string ARTICLE_TYPE_NAME { get; set; }
         public int CATEGORY_ID { get; set; }
-        public string? CATEGORY_NAME { get; set; } 
-
+        public string CATEGORY_NAME { get; set; }
         public int BRAND_ID { get; set; }
-        public string? BRAND_NAME { get; set; }
+        public string BRAND_NAME { get; set; }
         public int NEXT_SERIAL { get; set; }
         public string IMAGE_NAME { get; set; }
-
         public int NEW_ARRIVAL_DAYS { get; set; }
         public bool IS_STOPPED { get; set; }
-        //public string? STATUS { get; set; }
         public int SUPPLIER_ID { get; set; }
-        public bool IsComponent { get; set; } = false;
-        public int? ComponentArticleID { get; set; }
-
+        public string SupplierName { get; set; }
+        public bool IS_COMPONENT { get; set; }
+        public int? COMPONENT_ARTICLE_ID { get; set; }
+        public string ComponentArticleName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public List<Sizes> Sizes { get; set; }
     }
+
 
     public class ArticleListResponse
     {
         public int flag { get; set; }
         public string Message { get; set; } = string.Empty;
         public List<ArticleUpdate> Data { get; set; }
+    }
+    public class Sizes
+    {
+        public int ID { get; set; }
+        public string Size { get; set; }
+        public string OrderNo { get; set; }
+    }
+    public class ArticleSelectRequest
+    {
+        public int UnitID { get; set; }
+        public string ArtNo { get; set; }
+        public string Color { get; set; }
+        public int CategoryID { get; set; }
+        public decimal Price { get; set; }
     }
 
 
