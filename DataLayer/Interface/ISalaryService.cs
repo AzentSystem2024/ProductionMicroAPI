bdb636050b1072005468120a2f58229ab4bd9206
Vismaya V <vismaya@renazentit.com>
2025-07-18 11:20:08 +0530

    ADD SALARY GENERATE

---
+++ DataLayer/Interface/ISalaryService.cs
@@ -0,0 +1,12 @@
+﻿using MicroApi.Models;
+
+namespace MicroApi.DataLayer.Interface
+{
+    public interface ISalaryService
+    {
+        GenerateSalaryResponse GenerateSalary(List<Salary> salaryList);
+        SalaryLookupResponse GetSalaryLookup(SalaryLookupRequest request);
+
+
+    }
+}
---
+++ Models/Salary.cs
@@ -0,0 +1,35 @@
+﻿namespace MicroApi.Models
+{
+    public class Salary
+    {
+        public int TS_ID { get; set; }
+        public int COMPANY_ID { get; set; }
+        public int USER_ID { get; set; }
+
+    }
+    public class GenerateSalaryResponse
+    {
+        public int flag { get; set; }            
+        public string Message { get; set; }
+      
+    }
+    public class SalaryLookupRequest
+    {
+        public int COMPANY_ID { get; set; }
+    }
+    public class SalaryLookup
+    {
+        public int SALARY_BILL_NO { get; set; }
+        public DateTime SAL_MONTH { get; set; }
+        public string EMPLOYEE_NO { get; set; }
+        public string EMPLOYEE_NAME { get; set; }
+        public decimal WORKED_DAYS { get; set; }
+        public decimal NET_AMOUNT { get; set; }
+    }
+    public class SalaryLookupResponse
+    {
+        public int flag { get; set; }
+        public string Message { get; set; }
+        public List<SalaryLookup> Data { get; set; }
+    }
+}
