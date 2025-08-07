93664c48bcab6cf8fd28c2a46acea1da49cd510a
Bhavana-BS <bhavana@renazentit.com>
2025-08-07 15:34:02 +0530

    Depreciation list for fixed asset

---
+++ Controllers/DepreciationController.cs
@@ -0,0 +1,36 @@
+﻿using MicroApi.DataLayer.Interface;
+using MicroApi.DataLayer.Service;
+using MicroApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepreciationController : ControllerBase
+    {
+        private readonly IDepreciationService _depreciationService;
+
+        public DepreciationController (IDepreciationService depreciationService)
+        {
+            _depreciationService = depreciationService;
+        }
+        [HttpPost]
+        [Route("FixedAsset/list")]
+        public DepreciationResponse FixedAssetList()
+        {
+            DepreciationResponse response = new DepreciationResponse();
+            try
+            {
+                response = _depreciationService.GetFixedAssetsList();
+            }
+            catch (Exception ex)
+            {
+                response.Flag = 0;
+                response.Message = ex.Message;
+                response.Data = new List<FixedAssetLists>();
+            }
+            return response;
+        }
+    }
+}
---
+++ Models/Depreciation.cs
@@ -0,0 +1,29 @@
+﻿namespace MicroApi.Models
+{
+    public class Depreciation
+    {
+    }
+    public class FixedAssetLists
+    {
+        public int ID { get; set; }
+        public string CODE { get; set; }
+        public string DESCRIPTION { get; set; }
+        public string ASSET_TYPE { get; set; }
+        //public int ASSET_LEDGER_ID { get; set; }
+        public float ASSET_VALUE { get; set; }
+        public int USEFUL_LIFE { get; set; }
+        public decimal RESIDUAL_VALUE { get; set; }
+        public string PURCH_DATE { get; set; }
+        public string LAST_DEPR_DATE { get; set; }
+        public float NET_DEPRECIATION { get; set; }
+        public float CURRENT_VALUE { get; set; }
+        public bool IS_INACTIVE { get; set; }
+
+    }
+    public class DepreciationResponse
+    {
+        public int Flag { get; set; }
+        public string Message { get; set; }
+        public List<FixedAssetLists> Data { get; set; }
+    }
+}
