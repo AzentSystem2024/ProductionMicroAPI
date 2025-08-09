03c0da18c063b50171b08dee18024e7e6b0446b0
Vismaya V <vismaya@renazentit.com>
2025-08-09 15:30:25 +0530

    changes in misc receipts list

--- DataLayer/Service/MiscReceiptService.cs
+++ DataLayer/Service/MiscReceiptService.cs
@@ -285,7 +285,7 @@ namespace MicroApi.DataLayer.Service
                                     {
                                         TRANS_ID = transId,
                                         VOUCHER_NO = reader["VOUCHER_NO"]?.ToString(),
-                                        TRANS_DATE = reader["TRANS_DATE"]?.ToString(),
+                                        TRANS_DATE = reader["TRANS_DATE"] != DBNull.Value ? Convert.ToDateTime(reader["TRANS_DATE"]).ToString("dd-MM-yyyy") : null,
                                         PARTY_NAME = reader["PARTY_NAME"]?.ToString(),
                                         TRANS_TYPE = reader["TRANS_TYPE"] != DBNull.Value ? Convert.ToInt32(reader["TRANS_TYPE"]): 0,
                                         TRANS_STATUS = reader["TRANS_STATUS"] != DBNull.Value ? Convert.ToInt32(reader["TRANS_STATUS"])  : 0,
