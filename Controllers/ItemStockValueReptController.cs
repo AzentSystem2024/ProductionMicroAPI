f02c32baf1a62a4b4c5009cbb086b0d9f0da45c8
Bhavana-BS <bhavana@renazentit.com>
2025-09-15 14:16:06 +0530

    ITEMSTOCKVALUE REPORT API

---
+++ Controllers/ItemStockValueReptController.cs
@@ -0,0 +1,32 @@
+﻿using MicroApi.DataLayer.Interface;
+using MicroApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemStockValueReptController : ControllerBase
+    {
+        private readonly IItemStockValueReptService _itemStockValueReportService;
+        public ItemStockValueReptController(IItemStockValueReptService itemStockValueReportService)
+        {
+            _itemStockValueReportService = itemStockValueReportService;
+        }
+        [HttpPost("itemstockvaluereport")]
+        public ItemStockValueReportResponse GetItemStockValueReport(ItemStockValueReportRequest request)
+        {
+            var response = new ItemStockValueReportResponse();
+            try
+            {
+                response = _itemStockValueReportService.GetItemStockValueReport(request);
+            }
+            catch (Exception ex)
+            {
+                response.Flag = 0;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+    }
+}
---
+++ Models/ItemStockValueReport.cs
@@ -0,0 +1,29 @@
+﻿namespace MicroApi.Models
+{
+    public class ItemStockValueReport
+    {
+        public int ITEM_ID { get; set; }
+        public string MATRIXCODE { get; set; }
+        public string ITEMCODE { get; set; }
+        public string DESCRIPTION { get; set; }
+        public string DEPARTMENT { get; set; }
+        public string CATEGORY { get; set; }
+        public string SUBCATEGORY { get; set; }
+        public string BRAND { get; set; }
+        public decimal QUANTITY_AVAILABLE { get; set; }
+        public decimal COST { get; set; }
+        public decimal ITEM_VALUE { get; set; }
+    }
+    public class ItemStockValueReportRequest
+    {
+        public DateTime ASONDATE { get; set; }
+        public int STORE_ID { get; set; }
+    }
+
+    public class ItemStockValueReportResponse
+    {
+        public int Flag { get; set; }
+        public string Message { get; set; }
+        public List<ItemStockValueReport> ItemStockValueDetails { get; set; }
+    }
+}
