b3354f450c96cd08b90ec397dcd3e60a76c890a1
Vismaya V <vismaya@renazentit.com>
2026-04-10 10:38:52 +0530

    add promotion,tenders master

---
+++ DataLayer/Interface/IPromotionSchemaService.cs
@@ -0,0 +1,13 @@
+﻿using MicroApi.Models;
+
+namespace MicroApi.DataLayer.Interface
+{
+    public interface IPromotionSchemaService
+    {
+        public List<PromotionSchema> GetAllItemBrand();
+        public Int32 Insert(PromotionSchema promotionSchema);
+        public Int32 Update(PromotionSchema promotionSchema);
+        public PromotionSchema GetSchema(int id);
+        public bool DeleteSchema(int id);
+    }
+}
---
+++ DataLayer/Interface/ITendersService.cs
@@ -0,0 +1,12 @@
+﻿using MicroApi.Models;
+
+namespace MicroApi.DataLayer.Interface
+{
+    public interface ITendersService
+    {
+        public List<Tenders> GetAllTender();
+        public Int32 SaveData(Tenders company);
+        public Tenders GetItems(int id);
+        public bool DeleteTenders(int id);
+    }
+}
