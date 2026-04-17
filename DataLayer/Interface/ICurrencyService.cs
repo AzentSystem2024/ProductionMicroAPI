a9b34786b6256987974e1eb54490a7e1707a7dc2
Nibin-Basheer <nibinerikkil@gmail.com>
2026-04-17 16:09:40 +0530

    currency added

---
+++ DataLayer/Interface/ICurrencyService.cs
@@ -0,0 +1,12 @@
+﻿using MicroApi.Models;
+
+namespace MicroApi.DataLayer.Interface
+{
+    public interface ICurrencyService
+    {
+        public List<Currency> GetAllCurrency();
+        public Int32 SaveData(Currency company);
+        public Currency GetItems(int id);
+        public bool DeleteCurrency(int id);
+    }
+}
